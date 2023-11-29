f3fbd0e71b28be6799501f1c66302b4814096cb1
Max8Curtis <mcurtis01@outlook.com>
2023-11-29 18:51:53 +0000

    Distortion effects demo code added

--- data/character_translation_load.py
+++ data/character_translation_load.py
@@ -13,45 +13,29 @@ class DatasetLoad():
         
     def createCsv(self):
         toCsv = []
-        for root, files, name in os.walk(self.dir):
+        for root, files, name in os.walk(self.dir):      
             if root != self.dir:
                 for x in name:
-                    if '.png' in x:
+                    if '.png' in x:      
+                        # print(root)          
                         index = root[root.index("\\")+1:]
+                        print(index)
                         toCsv.append([os.path.join(root, x), int(index)])
                         
                         
         toCsv.sort(key=lambda x: x[1])
         toCsv = [x for x in toCsv if x[1] <= self.i]
+        # print(toCsv)
 
-        with open(os.path.join(self.dir, self.csv), "w", newline='') as f:
+        with open(os.path.join(self.dir, self.csv), "w+", newline='') as f:
             writer = csv.writer(f)
             for x in range(len(toCsv)):
                 writer.writerow(toCsv[x])
         
             f.close()
 
-    # def load(self):
-    #     for root, files, name in os.walk(self.dir):
-    #         if root != self.dir:
-    #             index = root[root.index("\\")+1:]
-    #             self.indexes.append(index)
+        return toCsv
 
-    #     int_indexes = [int(i) for i in self.indexes]
-    #     int_indexes.sort()
-    #     self.indexes = [str(i) for i in int_indexes]
 
-    #     for x in range(self.i):
-    #         dir_path = os.path.join(self.dir, self.indexes[x])
-    #         for root, files, name in os.walk(dir_path):
-    #             for file in name:
-    #                 im = Image.open(os.path.join(dir_path, file))
-    #                 # img_copy = img.copy()
-    #                 g = Grayscale(num_output_channels=1)
-    #                 im = g(im)
-    #                 img = np.asarray(im, dtype='float32')
-    #                 self.dataset.append(img)
-    #                 self.labels.append(x+1)
-    
-    # def getData(self):
-    #     return self.dataset, torch.from_numpy(np.asarray(self.labels, dtype='float32'))
+ds = DatasetLoad('./source', 2, 'trainData.csv').createCsv()
+print(ds)
