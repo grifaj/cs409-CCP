316c41d68796b9980939a75d14924e490e5c4774
Alfie Griffin <alfiegriffin100@gmail.com>
2024-04-07 20:54:10 +0100

    added ncnn library

--- Cpp_test/app/src/main/cpp/native-lib.cpp
+++ Cpp_test/app/src/main/cpp/native-lib.cpp
@@ -4,9 +4,10 @@
 #include <opencv2/highgui/highgui.hpp>
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/imgproc.hpp"
+#include <android/asset_manager.h>
+#include <android/asset_manager_jni.h>
 #include "enhance.h"
-
-cv::Mat captureImage(cv::Mat mat);
+#include "ncnn.h"
 
 extern "C"
 JNIEXPORT jstring JNICALL
@@ -24,7 +25,27 @@ Java_com_android_example_cpp_1test_MainActivity_validate(JNIEnv *env, jobject th
 }
 extern "C"
 JNIEXPORT void JNICALL
-Java_com_android_example_cpp_1test_CameraActivity_callBoundingBoxes(JNIEnv *env, jobject thiz, jlong image) {
+Java_com_android_example_cpp_1test_CameraActivity_callBoundingBoxes(JNIEnv *env, jobject thiz, jlong image, jobject assetManager) {
     cv::Mat* matImage=(cv::Mat*)image;
-    *matImage =  captureImage(*matImage);
+    AAssetManager* mgr = AAssetManager_fromJava(env, assetManager);
+    std::string pred_class = Detection(*matImage, mgr);
+    //*matImage =  captureImage(*matImage);
+}
+/**
+ * @brief Entry point of C++ code and MainActivity.java will call this function.
+ * @tparam env: JNIEnv pointer.
+ * @tparam bitmapIn: input image in bitmap format.
+ * @param assetManager: AssetManager object for loading NCNN model files in assets folder.
+ * @Return predicted class.
+ */
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_android_example_cpp_1test_CameraActivity_ImageClassification(
+        JNIEnv* env,
+        jobject,
+        jlong bitmapIn,
+        jobject assetManager) {
+    cv::Mat* matImage=(cv::Mat*)bitmapIn;
+    AAssetManager* mgr = AAssetManager_fromJava(env, assetManager);
+    std::string pred_class = Inference(*matImage, mgr);    // Image classification
+    return env->NewStringUTF(pred_class.c_str());
 }
