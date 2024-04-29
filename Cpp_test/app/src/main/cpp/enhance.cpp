ad829bc32bde828d4bb12428a6b15377b2a9bcba
Jonas <jonasnewbig@gmail.com>
2024-04-29 13:58:23 +0100

    comments to explain

--- Cpp_test/app/src/main/cpp/enhance.cpp
+++ Cpp_test/app/src/main/cpp/enhance.cpp
@@ -18,6 +18,9 @@ bool modelInitialisedFlag = false;
 bool detmodelInitialisedFlag = false;
 AAssetManager* mgr;
 
+
+//load translation model ncnn files into ncnn::net global variable
+//only used if an error occurs and clears the preloaded models
 void loadTranslationModel() {
     // Load model
     int ret = translationModel.load_param(mgr,"mobilenet_v3_large-sim-opt.param");
@@ -33,6 +36,25 @@ void loadTranslationModel() {
     modelInitialisedFlag = true;
 }
 
+//load detection model ncnn files into ncnn::net global variable
+//only used if an error occurs and clears the preloaded models
+void loadDetectionModel()
+{
+    int ret = detectionModel.load_param(mgr,"model.ncnn.param");
+    if (ret)
+    {
+        __android_log_print(ANDROID_LOG_ERROR, "load_param_error", "Failed to load the model parameters");
+    }
+    ret = detectionModel.load_model(mgr, "model.ncnn.bin");
+    if (ret)
+    {
+        __android_log_print(ANDROID_LOG_ERROR, "load_weight_error", "Failed to load the model weights");
+    }
+    detmodelInitialisedFlag = true;
+}
+
+//This function is used to preload the translation and detection model on app startup, so that it doesn't have to
+//be loaded when the user wants to detect
 void preloadModels(AAssetManager* manager) {
     auto beg = std::chrono::high_resolution_clock::now();
     mgr = manager;
@@ -63,6 +85,8 @@ void preloadModels(AAssetManager* manager) {
     detmodelInitialisedFlag = true;
 }
 
+//takes in a pointer to an image, converts it from RGBA (this will be the android phone formatting)
+//into a grayscale image. it then returns the grayscale version of the image.
 cv::Mat grayImage(cv::Mat* srcImg)
 {
     cv::Mat img;
@@ -74,6 +98,8 @@ cv::Mat grayImage(cv::Mat* srcImg)
     return grayImg;
 }
 
+//this takes in a pointer to an image, applies morphological closing and median blur to it
+//then returns the processed image
 cv::Mat preProcessImage(cv::Mat* srcImg)
 {
     cv::Mat grayDilate;
@@ -93,6 +119,8 @@ cv::Mat preProcessImage(cv::Mat* srcImg)
     return graySmoothed;
 }
 
+//takes in a gray image and converts it back into BGR formatting (it will still appear gray it
+//just now has three channels). Returns BGR formatted gray image.
 cv::Mat grayToBGR(cv::Mat* srcImg)
 {
     cv::Mat grayBGR;
@@ -101,6 +129,10 @@ cv::Mat grayToBGR(cv::Mat* srcImg)
     return grayBGR;
 }
 
+//this takes in an image and a scale factor variable to update.
+//It attempts to resize the image so the longer dimension becomes 512px and the shorter
+//dimension is scaled accordingly, the sf pointer is updated to keep this value for later use
+//the scaled image is returned
 cv::Mat resizeSF(cv::Mat *srcImg, float* sf)
 {
     cv::Mat srcScaled;
@@ -117,6 +149,9 @@ cv::Mat resizeSF(cv::Mat *srcImg, float* sf)
     return srcScaled;
 }
 
+//this takes an image and works out which side is shorter after the scaling that just happened
+//then scales the shorter side up to 512px to produce a 512x512 image with aspect ratio maintained
+//it returns the padded image
 cv::Mat padImage(cv::Mat *srcImg)
 {
     cv::Mat srcFinal;
@@ -143,42 +178,60 @@ cv::Mat padImage(cv::Mat *srcImg)
     return srcFinal;
 }
 
+//this takes in a source image, the original colour image, the scale factor used to scale in resizeSF
+//and a pointer to a vector containing bounding boxes and confidence scores of them
 void detectModel(cv::Mat *srcImg, cv::Mat *orig, float* sf, std::vector<cv::Rect>* boxes, std::vector<float>* confidences)
 {
+    //convert the image we want to perform detection on into ncnn format so model can use it
     ncnn::Mat input = ncnn::Mat::from_pixels(srcImg->data,
-                                             ncnn::Mat::PixelType::PIXEL_RGB,
+                                             ncnn::Mat::PixelType::PIXEL_BGR,
                                              srcImg->cols, srcImg->rows);
 
 
+    //normalise the image to mean of zero and std of 1
     float means[] = {0.0, 0.0, 0.0};
     float norms[] = {1.0/255.0, 1.0/255.0, 1.0/255.0};
     input.substract_mean_normalize(means, norms);
 
-    // Inference
+    //create extractor which takes input and gives output
     ncnn::Extractor extractor = detectionModel.create_extractor();
     extractor.input("in0", input);
     ncnn::Mat output;
     extractor.extract("out0", output);
 
+    //flatten the output
     ncnn::Mat out_flattened = output.reshape(output.w * output.h * output.c);
+    //outputs are given such that it gives all results of a specific type first and then goes
+    //to the next type. i.e. all box x's, all box y's, all box widths, all box heights, all box confidences.
 
+
+
+    //set up a confidence thresh to remove any boxes that aren't above this
     float confidenceThresh = 0.65;
+    //because result is row major we have to set up a method to read across columns for individual
+    //boxes and their scores. this is done by getting flattened shape divided by 5.
+    //then for each iteration we look at this index, this index + sec_size and so on...
     int sec_size = out_flattened.w / 5;
     for (int j=0; j<sec_size; j++)
     {
+        //if confidence score for this box is greater than confidence thresh
         if (out_flattened[j + (sec_size * 4)] > confidenceThresh)
         {
+            //get x,y,w,h from our results for a specific box
             float x = out_flattened[j] / *sf;
             float y = out_flattened[j + (sec_size)] / *sf;
             float w = out_flattened[j + (sec_size * 2)] / *sf;
             float h = out_flattened[j + (sec_size * 3)] / *sf;
 
+            //calculate top left of the box using what we have (x and y is center of box)
             int left = int((x - 0.5 * w));
             int top = int((y - 0.5 * h));
 
+            //ensure that box co-ordinates do not exceed bounds
             if (left > 0 && top > 0 && w > 0 && h > 0 &&
                 left+w < orig->cols && top+h < orig->rows)
             {
+                //add this box to our boxes and its confidence to list of scores
                 int width = int(w);
                 int height = int(h);
                 boxes->push_back(cv::Rect(left, top, width, height));
@@ -188,61 +241,59 @@ void detectModel(cv::Mat *srcImg, cv::Mat *orig, float* sf, std::vector<cv::Rect
     }
 }
 
-void loadDetectionModel()
-{
-    int ret = detectionModel.load_param(mgr,"model.ncnn.param");
-    if (ret)
-    {
-        __android_log_print(ANDROID_LOG_ERROR, "load_param_error", "Failed to load the model parameters");
-    }
-    ret = detectionModel.load_model(mgr, "model.ncnn.bin");
-    if (ret)
-    {
-        __android_log_print(ANDROID_LOG_ERROR, "load_weight_error", "Failed to load the model weights");
-    }
-    detmodelInitialisedFlag = true;
-}
-
+//this function takes in vector of boxes, and vector of their confidence scores
+//sorts the boxes using confidence score vector as a key
 void sortParallelVector(std::vector<cv::Rect>* vec, std::vector<float>* score_vec)
 {
+    //set up reference vectors to easily be able to index elements
     std::vector<cv::Rect>& vec_ref = *vec;
     std::vector<float>& score_ref = *score_vec;
 
+    //set up some new vectors to help sort
     std::vector<std::size_t> index_vec;
     std::vector<cv::Rect> vec_ordered;
     std::vector<float> score_vec_ordered;
 
+    //push back an index for each element we have in our vectors we want to sort
     for (std::size_t i = 0; i != vec->size(); ++i)
     {
         index_vec.push_back(i);
     }
 
+    //sort the indexes we just put into the vector above so that the indexes are in order of which
+    //index has the highest score in our score vector down to the lowest score in score vector
     std::sort(
             index_vec.begin(), index_vec.end(),
             [&](std::size_t a, std::size_t b) {return score_ref[a] > score_ref[b];});
 
+    //go through our index list, that index from original lists into our new ordered lists
     for (std::size_t i = 0; i != index_vec.size(); ++i)
     {
         vec_ordered.push_back(vec_ref[index_vec[i]]);
         score_vec_ordered.push_back(score_ref[index_vec[i]]);
     }
 
+    //assign pointers to look at new ordered vectors
     *vec = vec_ordered;
     *score_vec = score_vec_ordered;
 }
 
+//calculates intersection of two rectangles divided by the union of two rectangles
 float calculate_IOU(cv::Rect a, cv::Rect b)
 {
+    //get area of a and its bottom right co-ordinates
     float areaA = a.area();
 
     float areaA_br_x = a.br().x;
     float areaA_br_y = a.br().y;
 
+    //if its area is zero return 0
     if (areaA <= 0.0)
     {
         return 0.0;
     }
 
+    //same for rectangle b
     float areaB = b.area();
 
     if (areaB <= 0.0)
@@ -252,27 +303,36 @@ float calculate_IOU(cv::Rect a, cv::Rect b)
     float areaB_br_x = b.br().x;
     float areaB_br_y = b.br().y;
 
+    //calculate intersection by getting the tl and br points inside the intersection
     float intersection_left_x = std::max(a.tl().x, b.tl().x);
     float intersection_left_y = std::max(a.tl().y, b.tl().y);
     float intersection_bottom_x = std::min(areaA_br_x, areaB_br_x);
     float intersection_bottom_y = std::min(areaA_br_y, areaB_br_y);
 
+    //the calculate the width and height and calculate area from that
     float intersection_width = std::max(intersection_bottom_x - intersection_left_x, (float)0.0);
     float intersection_height = std::max(intersection_bottom_y - intersection_left_y, (float)0.0);
 
     float intersection_area = intersection_width * intersection_height;
 
+    //return intersection area divided by union area - intersection area
     return (float) intersection_area / (float) (areaA + areaB - intersection_area);
 }
 
+//calculate non-maximum suppression based on a threshold across a vector list of boxes and a vector list of confidence scores
+//place maximum boxes for an area into a new list called selected.
 void nms(std::vector<cv::Rect>* boxes, std::vector<float>* scores, std::vector<cv::Rect>* selected, float thresh)
 {
 
+    //sort the boxes so most confident boxes are first
     sortParallelVector(boxes, scores);
 
+    //create a reference to it so it can be easily indexed
     std::vector<cv::Rect>& boxes_ref = *boxes;
     //std::vector<float>& score_ref = *scores;
 
+    //create a new vector list called active which has a corresponding value for each box
+    //initially each box is considered active so true is pushed back for every position
     std::vector<bool> active;
 
     for (std::size_t i = 0; i != boxes->size(); i++)
@@ -280,30 +340,41 @@ void nms(std::vector<cv::Rect>* boxes, std::vector<float>* scores, std::vector<c
         active.push_back(true);
     }
 
+    //get number of still active boxes (which is just total size at this point)
     int num_active = active.size();
 
+    //set up a stop variable
     bool done = false;
 
+    //for each box...
     for (std::size_t i = 0; i != boxes->size(); i++)
     {
+        //check if it's active first
         if (active[i])
         {
+            //if it is then push this box into our selected boxes list (it should be
+            //the most confident box in a given area)
             cv::Rect box_a = boxes_ref[i];
             selected->push_back(box_a);
 
+            //for every other box after this one
             for (std::size_t j = i+1; j != boxes->size(); j++)
             {
+                //if this box is still active
                 if (active[j])
                 {
+                    //calculate it's IOU with outer loop box
                     cv::Rect box_b = boxes_ref[j];
 
                     float iou = calculate_IOU(box_a, box_b);
 
+                    //if it has too big an IOU then this box should be marked as inactive
                     if (iou > thresh)
                     {
                         active[j] = false;
                         num_active--;
 
+                        //if there are no more active boxes we are done
                         if (num_active <= 0)
                         {
                             done = true;
@@ -321,8 +392,12 @@ void nms(std::vector<cv::Rect>* boxes, std::vector<float>* scores, std::vector<c
     }
 }
 
+//this function takes in the original image as well as a rectangle box for the region we want
+//to binarise
+//it outputs a smaller image of the region of interest that has been binarised
 cv::Mat binariseBox(cv::Mat img, cv::Rect inBox)
 {
+    //convert to gray and smooth
     cv::Mat grayImg;
     cvtColor(img, grayImg, cv::COLOR_BGR2GRAY);
 
@@ -332,6 +407,7 @@ cv::Mat binariseBox(cv::Mat img, cv::Rect inBox)
     cv::Mat boxSmoothed;
     cv::medianBlur(boxImg, boxSmoothed, 5);
 
+    //perform otsu's thresholding
     cv::Mat threshBox;
 
     cv::threshold(boxSmoothed, threshBox, 0, 255, cv::THRESH_OTSU);
@@ -339,6 +415,8 @@ cv::Mat binariseBox(cv::Mat img, cv::Rect inBox)
     boxSmoothed.release();
     grayImg.release();
 
+    //if two more or more corners are black we should invert the image (i.e. the background has
+    //been made black in thresholding).
     int rows = threshBox.rows;
     int cols = threshBox.cols;
 
@@ -354,12 +432,15 @@ cv::Mat binariseBox(cv::Mat img, cv::Rect inBox)
         cv::bitwise_not(threshBox, threshBox);
     }
 
+    //convert thresholded image to BGR format so it has 3 channels all of the binary values
     cv::Mat threshBGR;
     cvtColor(threshBox, threshBGR, cv::COLOR_GRAY2BGR);
 
     return threshBGR;
 }
 
+//perform resizing and center cropping in line with mobilenetv3 translations before the image
+//goes into the model
 cv::Mat translationPreProcess(cv::Mat* srcImg)
 {
     cv::Mat binRoiR;
@@ -372,22 +453,30 @@ cv::Mat translationPreProcess(cv::Mat* srcImg)
     return binRoiR(roiBin).clone();
 }
 
+
+//this function takes in an input image and will assign values to the max and argmax variables
+//which determine the confidence of the prediction as well as which class was predicted
 void getTranslation(cv::Mat* srcImg, float* max, std::string* argMax)
 {
+    //convert input image into ncnn format
     ncnn::Mat input = ncnn::Mat::from_pixels(srcImg->data, ncnn::Mat::PIXEL_BGR2RGB, srcImg->cols,
                                              srcImg->rows);
 
+    //normalise the values according to mobilenet translations
     float means[] = {0.485f*255.f, 0.456f*255.f, 0.406*255.f};
     float norms[] = {1/0.229f/255.f, 1/0.224/255.f, 1/0.225f/255.f};
     input.substract_mean_normalize(means, norms);
 
+    //create extractor which takes in input and gives output
     // Inference
     ncnn::Extractor extractor = translationModel.create_extractor();
     //extractor.set_light_mode(true);
     extractor.input("input", input);
     ncnn::Mat output;
     extractor.extract("output", output);
+    //output in this example is an array of confidence scores
 
+    //step through each score to find the maximum score and class it belongs to
     *max = 0.0;
     for (int j = 0; j < output.w; j++) {
         if (output[j] > *max) {
@@ -397,11 +486,15 @@ void getTranslation(cv::Mat* srcImg, float* max, std::string* argMax)
     }
 }
 
+//this takes in two images, the region where character has been translated and that same region
+//on the image we are overwriting onto with the character overlay
 void overlayTranslation(cv::Mat roi, cv::Mat replaceroi, float* max, std::string* argMax)
 {
+    //check that prediciton is above confidence threshold
     float confThreshold = 0.7;
     if (*max >= confThreshold)
     {
+        //if it is then get character bitmap image
         std::string filename = "overlays/";
         filename.append(*argMax);
         filename.append(".bmp");
@@ -413,7 +506,7 @@ void overlayTranslation(cv::Mat roi, cv::Mat replaceroi, float* max, std::string
         AAsset_read(asset, buffer, size);
         AAsset_close(asset);
 
-        // convert file to rgb image
+        //convert bitmap image file into an rgb image
         cv::Mat rawData(1, size, CV_8UC1, (void *) buffer);
         cv::Mat decodedImage = imdecode(rawData, cv::IMREAD_COLOR);
 
@@ -421,17 +514,18 @@ void overlayTranslation(cv::Mat roi, cv::Mat replaceroi, float* max, std::string
         cvtColor(decodedImage, decodeColor, cv::COLOR_BGR2RGB);
         decodedImage.release();
 
-        //overlay image on rectangle
-
+        //resize our overlay to the region we are replacing size
         cv::Mat overlayImg;
         cv::resize(decodeColor, overlayImg, roi.size());
         decodeColor.release();
 
+        //create an alpha mask to ensure our overlay has alpha values set to full, making it fully visible
         cv::Mat alphaMask(roi.rows, roi.cols, CV_8UC1, cv::Scalar(255));
 
         cv::cvtColor(overlayImg, overlayImg, cv::COLOR_RGB2RGBA);
         cv::cvtColor(roi, roi, cv::COLOR_BGR2RGBA);
 
+        //overwrite alpha values of overlay with our mask
         std::vector<cv::Mat>channels(4);
         cv::split(overlayImg, channels);
 
@@ -439,16 +533,25 @@ void overlayTranslation(cv::Mat roi, cv::Mat replaceroi, float* max, std::string
 
         cv::merge(channels, overlayImg);
 
+        //add the overlay image and original region in a weighted fashion to the replacement image region
+        //overlay image has weight of 1, original region has weight of 0.
         addWeighted(overlayImg, 1, roi, 0, 0, replaceroi);
+
+
+        //when we overwrite the replacement region (which was a section of the image we want to put the overlay onto),
+        //it will propagate back to the original image it was taken from overwriting that region in itself as well
 
         overlayImg.release();
     }
 }
 
+//assign asset manager variable as the asset manager that got passed
 void assignManager(AAssetManager* manager){
     mgr = manager;
 }
 
+//this function is used for live mode capture and photo mode capture, it steps through all the above
+//functions in a pipeline fashion in order to produce an output image with translated characters overlaid
 cv::Mat captureImage(AAssetManager* manager, cv::Mat srcImg, int option) {
     auto beg = std::chrono::high_resolution_clock::now();
     assignManager(manager);
@@ -485,6 +588,10 @@ cv::Mat captureImage(AAssetManager* manager, cv::Mat srcImg, int option) {
         loadTranslationModel();
     }
 
+    //option parameter is used here so if the app is in live mode, the image we overlay the characters
+    //onto should be a blank and completely invisible image (alpha channel = 0)
+    //Using this alpha trick we can overlay a photo onto the user's view without obscuring the live
+    //camera view behind it and make it appear like translations are appearing in realtime
     cv::Mat overlay(srcImg.rows, srcImg.cols, CV_8UC4, cv::Scalar(0,0,0,0));
     cv::Mat replaceImg;
     if (option == 1)
@@ -520,6 +627,9 @@ cv::Mat captureImage(AAssetManager* manager, cv::Mat srcImg, int option) {
     return replaceImg;
 }
 
+//this function is used for draw mode capture, it steps through some of the above functions in
+//a pipeline fashion to translate the bounding box provided by the rectangle the user drew in draw
+//mode and produce an image where there is a character overlaid onto that region
 cv::Mat captureBoxImage(AAssetManager* manager, cv::Mat srcImg, int x, int y, int w, int h) {
     auto beg = std::chrono::high_resolution_clock::now();
     mgr = manager;
